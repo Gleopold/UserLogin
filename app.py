9d8a45518457bd52d1a59f0690d9348b5e268d3e
Gleopold <leopold.gabriel@gmail.com>
2022-01-05 14:43:57 +0100

    First commit, working
    Dela sve. Treba dodat user u DB i dodat duo.

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegistrationForm(Form):
+    username = StringField(validators=[InputRequired(), Length(
+        min = 4, max=20)], render_kw={"placeholder":"Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min = 4, max=20)], render_kw={"placeholder":"Password"})
+    submit = SubmitField("Register")
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
