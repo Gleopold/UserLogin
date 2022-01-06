449e577d21ef17fb61ec19b14bafe665be26fbbc
Gleopold <leopold.gabriel@gmail.com>
2022-01-06 16:57:50 +0100

    Login working
    Login working

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
+bcrypt = Bcrypt(app)
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
